566ac1e470a8bd44b0b682e4ce06fe7a041781e1
Eduardo Quirino <eduardo.quirino.oliveira@gmail.com>
2020-05-25 16:17:46 -0300

    created StudentSerializer and added student_list view

--- website/api/views.py
+++ website/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_except
+from rest_framework.parsers import JSONParser
+from api.Models import Student
+from api.serializers import StudentSerializer
 
-# Create your views here.
+@csrf_except
+def student_list(request):
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return JsonResponse(serializer, safe=False)
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = StudentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
