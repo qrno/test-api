4a88f0d3f30b201e9c32228dac858057d06c4f07
Eduardo Quirino <eduardo.quirino.oliveira@gmail.com>
2020-05-25 18:24:40 -0300

    added student_detail view

--- website/api/urls.py
+++ website/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('students/', views.student_list)
+    path('students/', views.student_list),
+    path('students/<int:pk>/', views.student_detail)
 ]
--- website/api/views.py
+++ website/api/views.py
@@ -17,3 +17,26 @@ def student_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def student_detail(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return JsonResponse(serializer.data)
+
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = StudentSerializer(student, data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+    if request.method == 'DELETE':
+        student.delete()
+        return HttpResponse(status=204)
