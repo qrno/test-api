4568471c9694854a621d47c834474b9f05ad8a16
Eduardo Quirino <eduardo.quirino.oliveira@gmail.com>
2020-06-05 15:22:44 -0300

    added blog app, with homepage

--- website/website/urls.py
+++ website/website/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    path('', include('api.urls')),
-    path('admin/', admin.site.urls)
+    path('students/', include('api.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls'))
 ]
